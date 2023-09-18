427ce464187bd1d20b95b40ccf01b79f36f69bef
Stas <postnikov.sa@ya.ru>
2023-09-18 20:20:55 +0700

    Update TokenController.php
    добавление комментариев

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -7,21 +7,31 @@ use Illuminate\Http\Request;
 
 class TokenController extends Controller
 {
+    /**
+     * Отображает форму для создания нового API-токена.
+     *
+     * @return \Illuminate\Contracts\View\View
+     */
     public function create()
     {
         return view('create-token');
     }
 
+    /**
+     * Создает новый API-токен для указанного пользователя.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function store(Request $request)
     {
-        // Проверка аутентификации пользователя (вы можете добавить проверку прав доступа)
-
         $user = User::find($request->user_id);
 
         if (!$user) {
             return redirect()->route('create.token')->with('error', 'Пользователь с указанным ID не найден.');
         }
 
+        // Создание нового API-токена для пользователя
         $token = $user->createToken($request->name)->accessToken;
 
         return redirect()->route('create.token')->with('message', 'Токен успешно создан: ' . $token);
