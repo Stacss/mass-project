6ae882240e18a80279e973d6619b6a0412720dfa
Stas <postnikov.sa@ya.ru>
2023-09-18 20:18:06 +0700

    Update RequestController.php
    Добавлены комментарии

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -9,14 +9,22 @@ use Illuminate\Support\Facades\Validator;
 
 class RequestController extends Controller
 {
+    /**
+     * Создать новую заявку пользователя.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function store(Request $request)
     {
+        // Проверка входных данных на валидность
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
             'message' => 'required|string',
         ]);
 
+        // Если входные данные не прошли валидацию, вернуть ошибку с кодом 400 (Bad Request)
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
@@ -29,8 +37,7 @@ class RequestController extends Controller
             'message' => $request->input('message'),
         ]);
 
-        // нужно добавить уведомление на почту
-
-        return response()->json(['message' => 'Заявка успешно создана'], 201);
+        // Вернуть успешный ответ с кодом 201 (Created) и сообщением о создании заявки
+        return response()->json(['message' => 'Заявка успешно создана', 'request' => $newRequest], 201);
     }
 }
